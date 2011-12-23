8fd72d12598c28c4c977c1f06571779696892bbb
Eric W. Wallace <e@ewall.org>
2011-12-23 12:00:41 -0500

    added support for multiple domains

--- Backup-GPOs.ps1
+++ Backup-GPOs.ps1
@@ -6,23 +6,47 @@ Backup-GPOs.ps1 -- by Eric Wallace <wallae@mmc.org>, December 2011
 
 Automated backup of Group Policy objects.
 
-.REQUIREMENTS
 
-This script must be run from an account with Domain Admins permissions on the target domain.
+.NOTES
 
-.PARAMETERS
+This script must be run from an account with Domain Admins permissions on the target domain(s).
 
-# TODO: document parameters
 
-.EXAMPLES
+.PARAMETER domainLst
 
-# TODO: document examples
+Domain name, or a list of domain names separated by commas only (no spaces!)
+
+
+.PARAMETER searchTxt
+
+A word or part of a word that should be in the GPO's display name. If you use this search option,
+only the GPOs matching this term will be backed up.
+
+
+.PARAMETER backupDir
+
+Directory for saving the GPO backups. The folder structure will be created automatically.
+
+
+.EXAMPLE
+
+Backup all GPOs from the current user's domain to the default location:
+  .\Backup-GPOs.ps1
+
+Backup all GPOs for multiple domains:
+  .\Backup-GPOs.ps1 mehealth.org,mmcf.mehealth.org,mhr.mehealth.org
+
+Backup only GPOs with the word "test" in their name:
+  .\Backup-GPOs.ps1 mmcf.mehealth.org test
+  
+Backup all GPOs to a specific directory (note that the search term is blank):
+  .\Backup-GPOs.ps1 mmcf.mehealth.org "" C:\My_GPO_Backups
 
 #>
 
 # configure via parameters
 param  (
-  [string]$domain    = ( [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain() ).Name,
+  [string]$domainLst    = ( [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain() ).Name,
   [string]$searchTxt = "",
   [string]$backupDir = "C:\GPOBackups"
 )
@@ -36,55 +60,61 @@ if (-Not (Test-Path $backupDir -PathType container)) { mkdir $backupDir | Out-Nu
 
 # TODO: what to do with output?
 "### Backup-GPOs.ps1 ###"
-" - Domain: " + $domain
-
-# query GPOs
 $gpm = New-Object -comObject GPMgmt.GPM
 $k = $gpm.GetConstants()
-$dom = $gpm.GetDomain($domain, "","")
-$sc = $gpm.CreateSearchCriteria() #empty criteria fetches all
 
-# custom search criteria
-If ($searchTxt -Ne "") {
-  $sc.add($k.SearchPropertyGPODisplayName,$k.SearchOpContains, $searchTxt)
-  " - GPO name contains: " + $searchTxt
-}
+# outer loop: each domain
+$domainLst.Split(", ") | Foreach-Object {
+  $domain = $_
+  " - Domain: " + $domain
 
-# loop-de-loop
-$dom.SearchGPOs($sc) | ForEach-Object {
-  # first-level folder structure
-  $bupath = Join-Path $backupDir $domain
-  If (-Not (Test-Path $bupath -PathType container)) {
+  # query GPOs
+  $dom = $gpm.GetDomain($domain, "","")
+  $sc = $gpm.CreateSearchCriteria() #empty criteria fetches all
+
+  # custom search criteria
+  If ($searchTxt -Ne "") {
+    $sc.add($k.SearchPropertyGPODisplayName,$k.SearchOpContains, $searchTxt)
+    " - GPO name contains: " + $searchTxt
+  }
+
+  # inner loop-de-loop: each GPO
+  $dom.SearchGPOs($sc) | ForEach-Object {
+    # first-level folder structure
+    $bupath = Join-Path $backupDir $domain
+    If (-Not (Test-Path $bupath -PathType container)) {
+      mkdir $bupath | Out-Null
+    }
+
+    # second-level folder structure
+    $gponame = $_.DisplayName
+    $bupath = Join-Path $bupath $gponame
+    If (-Not (Test-Path $bupath -PathType container)) {
+      mkdir $bupath | Out-Null
+    }
+    
+    # save report here
+    If ($createReports) {
+      $reportContent = $_.GenerateReport($k.ReportHTML)
+      $reportFile = Join-Path $bupath ($gponame + '_' + $today + '.html')
+      $reportContent.result | Out-File $reportFile
+    }
+    
+    # third-level folder structure
+    $bupath = Join-Path $bupath $today
+    If (Test-Path $bupath -PathType container) {
+      # note that if you've already run this today, this run will replace the earlier backup!
+      Remove-Item -Recurse -Force $bupath
+    }
     mkdir $bupath | Out-Null
+    
+    # actually do the backup
+    $comment = 'Domain: ' + $domain + ' | GPO: ' + $gponame + ' | Date: ' + $today
+    $result = $_.Backup( $bupath, $comment )
+    $result.result # default pretty-print of COM object properties and values
+    ""
   }
 
-  # second-level folder structure
-  $gponame = $_.DisplayName
-  $bupath = Join-Path $bupath $gponame
-  If (-Not (Test-Path $bupath -PathType container)) {
-    mkdir $bupath | Out-Null
-  }
-  
-  # save report here
-  If ($createReports) {
-    $reportContent = $_.GenerateReport($k.ReportHTML)
-    $reportFile = Join-Path $bupath ($gponame + '_' + $today + '.html')
-    $reportContent.result | Out-File $reportFile
-  }
-  
-  # third-level folder structure
-  $bupath = Join-Path $bupath $today
-  If (Test-Path $bupath -PathType container) {
-    # note that if you've already run this today, this run will replace the earlier backup!
-    Remove-Item -Recurse -Force $bupath
-  }
-  mkdir $bupath | Out-Null
-  
-  # actually do the backup
-  $comment = 'Domain: ' + $domain + ' | GPO: ' + $gponame + ' | Date: ' + $today
-  $result = $_.Backup( $bupath, $comment )
-  $result.result # default pretty-print of COM object properties and values
-  ""
 }
 
 "Finished!"
